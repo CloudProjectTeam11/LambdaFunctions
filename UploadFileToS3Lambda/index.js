6f3c22f88151174a68c0668620c2eae2c84ccf4e
ForLoop111 <vlada.devic.2001@gmail.com>
2023-05-04 10:36:34 +0200

    Update UploadFileToS3Function

--- UploadFileToS3Lambda/index.js
+++ UploadFileToS3Lambda/index.js
@@ -1,13 +1,26 @@
-const multipart = require("parse-multipart")
-const AWS = require("aws-sdk")
+const multipart = require("parse-multipart");
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
 
 var s3 = new AWS.S3();
 
+async function getS3Object(objectKey) {
+    const params = {
+      Bucket: "demo-app-content",
+      Key: objectKey
+    };
+  
+    try {
+      const data = await s3.headObject(params).promise();
+      return data
+    } catch (error) {
+        return undefined;
+    }
+  }
+
 exports.handler = async (event, context, callback) => {
-    let bodyBuffer = new Buffer(event['body'].toString(), 'base64');
+    let bodyBuffer = Buffer.from(event['body'], 'base64');
     let boundary = multipart.getBoundary(event.headers["content-type"]);
-    
-    console.log(event);
 
     let user = event.requestContext.authorizer["X-User-Id"];
     let parts = multipart.Parse(bodyBuffer, boundary);
@@ -20,16 +33,41 @@ exports.handler = async (event, context, callback) => {
 
     let decodedImage = Buffer.from(parts[0].data, 'base64');
     
+    const data = await getS3Object(filename);
+    console.log(data);
     let params = {
         "Body" : decodedImage,
         "Bucket": "demo-app-content",
-        "Key" : filename
+        "Key" : filename,
     }
-
+    let fileId = "";
+    if(data == undefined){
+        fileId = uuid.v1();
+        params["Metadata"] = {
+            "file-id" : fileId,
+            "user-id" : user,
+            "is-changed" : "false"
+        };
+    }else{        
+        fileId = data.Metadata["file-id"];
+        params["Metadata"] = {
+            "file-id" : fileId,
+            "user-id" : user,
+            "is-changed":"true"
+        }
+    }
+    console.log(params);
     const result = await s3.upload(params).promise();
+
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        headers: {
+            "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Credentials" : "true",
+            "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+            "Access-Control-Allow-Headers" : "Access-Control-Allow-Headers, authenticationToken, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        },
+        body: JSON.stringify({"fileID": fileId})
     };
     return response;
 };
