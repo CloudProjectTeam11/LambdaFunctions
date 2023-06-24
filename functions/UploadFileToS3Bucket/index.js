a201fd7b26e427bf4c19930b093477927b7c3178
vdevic01 <vlada.devic.2001@gmail.com>
2023-06-24 22:52:18 +0200

    Fixes upload files lambda

--- functions/UploadFileToS3Bucket/index.js
+++ functions/UploadFileToS3Bucket/index.js
@@ -5,6 +5,7 @@ const uuid = require("functions/UploadFileToS3Bucket/node_modules/uuid");
 var s3 = new AWS.S3();
 var dynamodb = new AWS.DynamoDB();
 const userFilesBucketName = process.env.USER_FILES_BUCKET;
+const albumsTable = process.env.ALBUMS_TABLE
 
 async function getS3Object(objectKey) {
     const params = {
@@ -23,20 +24,23 @@ async function getS3Object(objectKey) {
 exports.handler = async (event, context, callback) => {
     let bodyBuffer = Buffer.from(event['body'], 'base64');
     let boundary = multipart.getBoundary(event.headers["content-type"]);
-
     let user = event.requestContext.authorizer["X-User-Id"];
     let parts = multipart.Parse(bodyBuffer, boundary);
     let album = event.queryStringParameters["album"];
     let filename = parts[0].filename;
 
     let dynamoParams = {
-        TableName: "AlbumsTable",
-        Key: "album"
+        TableName: albumsTable,
+        Key: {
+            album_id: { S: album },
+        }
     };
-    const albumResult = await dynamodb.get(dynamoParams).promise();
-    if(result.Item){
-        const albumObj = result.Item;
-        if(album.Owner != user){
+    const albumResult = await dynamodb.getItem(dynamoParams).promise();
+    if(albumResult.Item){
+        const albumObj = albumResult.Item;
+        console.log(albumObj);
+        console.log(user);
+        if(albumObj["album_owner"]["S"] != user){
             return {
                 statusCode: 403,
                 headers: {
